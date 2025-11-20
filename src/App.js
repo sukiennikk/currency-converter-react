84d2f815fc073fea26bf24db9d6aafb487d8238e
Katarzyna Sukiennik <katarzyna.sukiennik99@gmail.com>
2025-11-20 17:17:35 +0100

    initialize react project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/style.css";
+import CurrencyConverter from "./components/CurrencyConverter";
+
+function App() {
+  return (
+    <div className="element">
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
